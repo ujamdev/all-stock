1ce1c946df76c9df70b99ccab814f31b0e937fdb
ujamdev <ujamdev@gmail.com>
2024-01-10 20:55:44 +0900

    feat: 애플리케이션 초기화 및 서버 클래스 생성

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+class Server {
+  private readonly configService: ConfigService;
+  private readonly port;
+
+  constructor(private readonly application: NestExpressApplication) {
+    this.configService = application.get(ConfigService);
+    this.port = this.configService.get('server.port') || 3000;
+  }
+
+  async bootstrap() {
+    await this.application.listen(this.port);
+    console.log(`listening on port ${this.port}`);
+  }
 }
-bootstrap();
+
+async function initialize(): Promise<void> {
+  const application = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+  const server = new Server(application);
+
+  await server.bootstrap();
+}
+
+initialize().catch((error) => Logger.error(error));
